35328ab3d6bc14872fab0a26439d570366235ca9
DavidJauregu123 <98029546+DavidJauregu123@users.noreply.github.com>
2022-06-01 12:31:48 -0500

    Add files via upload

---
+++ Calificacion.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+
+int main()
+{
+    int x=0;
+    printf("Escribe tu calificación: \n");
+    scanf("%d", &x);
+    if (x>6)
+        printf("Aprobado \n");
+    
+    else 
+        printf("Te falta odio \n");
+    return 0;
+    
+    }
+
+
