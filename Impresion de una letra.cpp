b3da5acba2722854cefbfd6b2c637a7f5d0c24d4
DavidJauregu123 <98029546+DavidJauregu123@users.noreply.github.com>
2022-06-01 12:03:27 -0500

    Add files via upload

---
+++ Impresion de una letra.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char a = 0;
+    printf("Escribe una letra: \n");
+    scanf("%c", &a);
+    printf("Este es tu letra: %c \n", a);
+
+    return 0;
+}
