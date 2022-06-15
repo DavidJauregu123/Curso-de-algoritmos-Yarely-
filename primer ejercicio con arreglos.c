6dda7a0ebc3f21b465f1ba7db4db39ce1d604946
DavidJauregu123 <98029546+DavidJauregu123@users.noreply.github.com>
2022-06-15 11:37:26 -0500

    Add files via upload

---
+++ primer ejercicio con arreglos.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    
+ int x[5];
+ 
+ 
+ 
+ printf("Escribe el primer numero \n");
+ scanf("%d", &x[0]); 
+ 
+  printf("Escribe el segundo numero \n");
+ scanf("%d", &x[1]);
+ 
+  printf("Escribe el tercer numero \n");
+ scanf("%d", &x[2]);
+ 
+  printf("Escribe el cuarto  numero \n");
+ scanf("%d", &x[3]);
+ 
+  printf("Escribe el quinto numero \n");
+ scanf("%d", &x[4]);
+ 
+ 
+ printf("tu primer numero fue: %d \n", x[0]);
+ printf("tu segundo numero fue:%d \n", x[1]);
+ printf("tu tercer numero fue: %d \n", x[2]);
+ printf("tu cuarto numero fue: %d \n", x[3]);
+ printf("tu quinto numero fue: %d \n", x[4]);
+
+    
+    return 0;
+}
+ 
+ 
